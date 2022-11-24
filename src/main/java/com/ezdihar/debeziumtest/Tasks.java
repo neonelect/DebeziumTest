2fcbb7399aab0839ffce6d319f838d76d0af7e26
Łukasz Włodarski <lukaszwlodarski@JIT-0001189.local>
2022-11-24 17:27:08 +0100

    Commit

--- src/main/java/com/ezdihar/debeziumtest/Tasks.java
+++ src/main/java/com/ezdihar/debeziumtest/Tasks.java
@@ -1,13 +1,14 @@
 package com.ezdihar.debeziumtest;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.math.BigInteger;
 
 @Entity
 public class Tasks {
 
     @Id
+    @GeneratedValue(strategy= GenerationType.AUTO, generator="my_entity_seq_gen")
+    @SequenceGenerator(name="my_entity_seq_gen", sequenceName="MY_ENTITY_SEQ")
     private Long id;
 
     private String text;
